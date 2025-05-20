54bc1dd19d7f7b7e14872c7954cd5cb8f7a3570c
Samuel Horsfield <shorsfield@hl-codon-08-02.ebi.ac.uk>
2025-05-20 12:06:48 +0100

    Ensures correct update of cluster representatives

--- scripts/merge_mmseqs2_clusters_update.py
+++ scripts/merge_mmseqs2_clusters_update.py
@@ -8,8 +8,10 @@ import os
 def merge_mmseqs2(rep_files, prev_clusters, clusters):
     # read in cluster file
     with open(prev_clusters, 'rb') as handle:
-        output_dicts = pickle.load(handle)
-        prev_rep_to_cluster, prev_cluster_to_rep = output_dicts
+         output_dicts = pickle.load(handle)
+         prev_rep_to_cluster, prev_cluster_to_rep = output_dicts
+
+    #prev_rep_to_cluster = {"old1": "old1", "old2": "old2", "old3": "old3", "old4": "old4", "old5": "old5", "old6": "old6"}
 
     # sort based on batch number
     rep_files = natsorted(rep_files)
@@ -32,7 +34,7 @@ def merge_mmseqs2(rep_files, prev_clusters, clusters):
                 if not line:
                     break
 
-                split_line = line.rstrip().split("\t")
+                split_line = line.rstrip().split(" ")
                 rep = split_line[0]
                 seq = split_line[1]
 
@@ -42,33 +44,39 @@ def merge_mmseqs2(rep_files, prev_clusters, clusters):
                 # fine, old rep matches itself
                 if rep in prev_rep_to_cluster and seq in prev_rep_to_cluster and rep == seq:
                     key, entry = (rep, seq)
+                    #print(f"Option1: {(key, entry)}")
                 
                 # fine, new sequence added to old rep
                 elif rep in prev_rep_to_cluster and seq not in prev_rep_to_cluster:
                     key, entry = (rep, seq)
+                    #print(f"Option2: {(key, entry)}")
                 
                 # fine, completely new cluster
                 elif rep not in prev_rep_to_cluster and seq not in prev_rep_to_cluster:
                     key, entry = (rep, seq)
+                    #print(f"Option3: {(key, entry)}")
 
                 # problem, merging of two old reps
                 elif rep in prev_rep_to_cluster and seq in prev_rep_to_cluster and rep != seq:
-                    merged_old_reps.add((rep, seq))
+                    #merged_old_reps.add((rep, seq))
+                    #print(f"Option4: {(rep, seq)}")
                     continue
 
                 # problem, new seq added as new rep, potential merge of multiple old reps
                 elif rep not in prev_rep_to_cluster and seq in prev_rep_to_cluster:
                     # if already present, indicates multiple old reps have been merged
-                    if seq not in rep_swap:
+                    if rep not in rep_swap:
                         rep_swap[rep] = seq
                     else:
                         # add "lowest" to be the representative
+                        old_swap = rep_swap[rep]
                         if rep_swap[rep] < seq:
                             rep_swap[rep] = seq
                         
-                        print(f"MERGE new_rep: {rep} old_rep {seq}")
+                        print(f"MERGE new_rep: {rep}, old_rep: {seq}, prev_old_rep: {old_swap}")
                     
                     key, entry = (rep_swap[rep], rep)
+                    #print(f"Option5: {(key, entry)}")
                     
                 # if rep has not been seen before, add to new cluster
                 if key not in rep_to_cluster:
@@ -91,6 +99,8 @@ def merge_mmseqs2(rep_files, prev_clusters, clusters):
                         del cluster_to_rep[entry]
                     except KeyError:
                         pass
+        #print(f"rep_to_cluster: {rep_to_cluster}")
+        #print(f"cluster_to_rep: {cluster_to_rep}")
         print("Finished: {}".format(rep_file))
 
     print("No. clusters: {}".format(str(len(cluster_to_rep))))
@@ -99,4 +109,8 @@ def merge_mmseqs2(rep_files, prev_clusters, clusters):
     with open(clusters, 'wb') as handle:
         pickle.dump(output_dicts, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
+#merge_mmseqs2(["/hps/software/users/jlees/shorsfield/software/WTBcluster/scripts/test_batch1.tsv", "/hps/software/users/jlees/shorsfield/software/WTBcluster/scripts/test_batch2.tsv"], None, "test_clustering.txt")
+#print("Finished forward")
+#merge_mmseqs2(["/hps/software/users/jlees/shorsfield/software/WTBcluster/scripts/test_batch2.tsv", "/hps/software/users/jlees/shorsfield/software/WTBcluster/scripts/test_batch1.tsv"], None, "test_clustering.txt")
+#print("Finished reverse")
 merge_mmseqs2(snakemake.input.infiles, snakemake.input.prev_clusters, snakemake.output.clusters)
