8a75292da626ba667ffe5a1391006141704a23f1
Samuel Horsfield <shorsfield@hl-codon-08-02.ebi.ac.uk>
2025-05-20 12:24:47 +0100

    Completes update workflow

---
+++ scripts/update_token_db.py
@@ -0,0 +1,73 @@
+import Bio.SeqIO
+import pyrodigal
+import gzip
+import os
+
+from pathlib import Path
+import argparse
+import os
+import pickle
+#from multiprocessing import Pool, Manager
+#from functools import partial
+import rocksdb
+import math
+
+def update_token_db(cluster_file, out_db, in_reps, out_reps):
+
+    # read in representative sequences and their token
+    with open(in_reps, 'rb') as handle:
+        reps_dict, rep_to_token = pickle.load(handle)
+
+    # dictionary mapping each gene to a given cluster token, update in place
+    opts = rocksdb.Options()
+    opts.max_open_files = 300000000
+    opts.max_bytes_for_level_base = 209715200 #(default = 10485760)
+    opts.target_file_size_base = math.ceil(opts.max_bytes_for_level_base / 10) #(default = 2097152)
+    opts.target_file_size_multiplier = 2 #(default = 1)
+
+    gene_tokens = rocksdb.DB(out_db, opts)
+
+    # dictionary of representative sequences and their token
+    reps_dict = {}
+    rep_to_token = {}
+
+    #start at len(rep_to_token) + 1 as cannot assign negative 0
+    token = len(rep_to_token) + 1
+    print("Generating token dictionaries...")
+    counter = 0
+    with open(cluster_file, "r") as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+
+            split_line = line.rstrip().split("\t")
+            split_rep = split_line[0]
+            split_seq = split_line[1]
+
+            rep = split_rep
+            seq = split_seq
+
+            # allows use of non-sorted list
+            if rep not in rep_to_token:
+                token += 1
+                rep_to_token[rep] = token
+            
+            current_token = rep_to_token[rep]
+            reps_dict[current_token] = rep
+            
+            # add sequence to cluster
+            gene_tokens.put(seq.encode(), str(current_token).encode())
+            counter += 1
+            if counter % 1000000 == 0:
+                print("At index: {}".format(counter))
+
+    # save data as pickle
+    print("Saving token dictionaries...")
+
+    with open(out_reps, "wb") as f:
+        pickle.dump((reps_dict, rep_to_token), f)
+    
+    print("Saved token dictionaries.")
+
+generate_token_db(snakemake.input.clusters, snakemake.input.out_db, snakemake.input.in_reps, snakemake.output.out_reps)
